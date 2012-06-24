1b46c0e3c857025a53222973e1e8a4f367ea1ac1
Dimitri Fontaine <dim@tapoueh.org>
2012-06-24 22:33:44 +0200

    Clean up useless nodes and comments.

--- src/test/regress/sql/type_sanity.sql
+++ src/test/regress/sql/type_sanity.sql
@@ -104,7 +104,7 @@ WHERE p1.typinput = p2.oid AND p1.typtype in ('b', 'p') AND NOT
 
 -- As of 8.0, this check finds refcursor, which is borrowing
 -- other types' I/O routines
--- As of 9.2, this also finds cmdtrigger.
+-- As of 9.3, this also finds event_trigger.
 SELECT p1.oid, p1.typname, p2.oid, p2.proname
 FROM pg_type AS p1, pg_proc AS p2
 WHERE p1.typinput = p2.oid AND p1.typtype in ('b', 'p') AND NOT
@@ -131,7 +131,7 @@ ORDER BY 1;
 
 -- As of 8.0, this check finds refcursor, which is borrowing
 -- other types' I/O routines
--- In 9.2 cmdtrigger is borrowing trigger routines
+-- In 9.3 event_trigger is borrowing trigger routines
 SELECT p1.oid, p1.typname, p2.oid, p2.proname
 FROM pg_type AS p1, pg_proc AS p2
 WHERE p1.typoutput = p2.oid AND p1.typtype in ('b', 'p') AND NOT
